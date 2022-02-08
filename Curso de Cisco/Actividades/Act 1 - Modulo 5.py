906dadc397853e95470504216776ae3ae919dcac
tomasfriz <tomyagustinf@gmail.com>
2022-02-08 15:10:36 -0300

    Terminado definitivamente

---
+++ Curso de Cisco/Actividades/Act 1 - Modulo 5.py
@@ -0,0 +1,17 @@
+﻿def readint(prompt, min, max):
+    ok = False
+    while not ok:
+        try:
+            value = int(input(prompt))
+            ok = True
+        except ValueError:
+            print("Error: entrada incorrecta")
+        if ok:
+            ok = value >= min and value <= max
+        if not ok:
+            print("Error: el valor no esta dentro del rango permitido (" + str(min) + ".." + str(max) + ")")
+    return value;
+
+v = readint("Ingresa un número entre -10 a 10: ", -10, 10)
+
+print("El numero es:", v)
